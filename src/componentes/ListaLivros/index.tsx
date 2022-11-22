2be1a71ecf27116500f6aa34f7f46f80d88e6ddf
Vinicios Neves <vinny@macbook.lan>
2022-11-22 22:51:52 +0000

    fim aula 2

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from "@apollo/client"
+import { AbBotao, AbCampoTexto } from "ds-alurabooks"
+import { useState } from "react"
 import { ICategoria } from "../../interfaces/ICategoria"
 import { ILivro } from "../../interfaces/ILivro"
 import CardLivro from "../CardLivro"
@@ -10,8 +12,8 @@ interface ListaLivrosProps {
 }
 
 const OBTER_LIVROS = gql`
-  query ObterLivros {
-    livros {
+  query ObterLivros($categoriaId: Int, $titulo: String) {
+    livros(categoriaId: $categoriaId, titulo: $titulo) {
       id
       slug
       titulo
@@ -26,11 +28,35 @@ const OBTER_LIVROS = gql`
 
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
 
-    const { data } = useQuery<{ livros: ILivro[]}>(OBTER_LIVROS)
+    const [textoBusca, setTextoDaBusca] = useState('')
+
+    const { data, refetch } = useQuery<{ livros: ILivro[]}>(OBTER_LIVROS, {
+        variables: {
+            categoriaId: categoria.id
+        }
+    })
+
+    const buscarLivros = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        if (textoBusca) {
+            refetch({
+                categoriaId: categoria.id,
+                titulo: textoBusca
+            })
+        }
+    }
 
     // const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
-    return <section className="livros">
-        {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+    return <section>
+        <form onSubmit={buscarLivros} style={{ maxWidth: '80%', margin: '0 auto', textAlign: 'center' }}>
+            <AbCampoTexto value={textoBusca} onChange={setTextoDaBusca} placeholder='Digite o título'/>
+            <div style={{ marginTop: '16px' }}>
+                <AbBotao texto="Buscar"/>
+            </div>
+        </form>
+        <div className="livros">
+            {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+        </div>
     </section>
 }
 
