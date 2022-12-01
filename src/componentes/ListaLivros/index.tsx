187da74559977afb87cb3cd117b61e9b157bc6d3
Vinicios Neves <vinny@macbook.lan>
2022-12-01 19:51:45 +0000

    fim aula 4

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,8 +1,8 @@
 import { useReactiveVar } from "@apollo/client"
-import { AbBotao, AbCampoTexto } from "ds-alurabooks"
-import { useState } from "react"
+import { AbCampoTexto } from "ds-alurabooks"
+import { useEffect, useState } from "react"
 import { useLivros } from "../../graphql/livros/hooks"
-import { livrosVar } from "../../graphql/livros/state"
+import { filtroLivrosVar, livrosVar } from "../../graphql/livros/state"
 import { ICategoria } from "../../interfaces/ICategoria"
 import CardLivro from "../CardLivro"
 
@@ -16,28 +16,25 @@ const ListaLivros = ({ categoria }: ListaLivrosProps) => {
 
     const [textoBusca, setTextoDaBusca] = useState('')
 
+    useEffect(() => {
+        filtroLivrosVar({
+            ...filtroLivrosVar(),
+            titulo: textoBusca.length >= 3 ? textoBusca : ''
+        })
+    }, [textoBusca])
+
+    filtroLivrosVar({
+        ...filtroLivrosVar(),
+        categoria,
+    })
+    
     const livros = useReactiveVar(livrosVar);
-    console.log('livros =>', livros)
 
-    useLivros(categoria)
+    useLivros()
 
-    const buscarLivros = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-        if (textoBusca) {
-            // refetch({
-            //     categoriaId: categoria.id,
-            //     titulo: textoBusca
-            // })
-        }
-    }
-
-    // const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
     return <section>
-        <form onSubmit={buscarLivros} style={{ maxWidth: '80%', margin: '0 auto', textAlign: 'center' }}>
+        <form style={{ maxWidth: '80%', margin: '0 auto', textAlign: 'center' }}>
             <AbCampoTexto value={textoBusca} onChange={setTextoDaBusca} placeholder='Digite o título'/>
-            <div style={{ marginTop: '16px' }}>
-                <AbBotao texto="Buscar"/>
-            </div>
         </form>
         <div className="livros">
             {livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
